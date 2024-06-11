8a632596026ce021277aa661741a8717fa0fa5dd
SSPENELOPE <typoepping@gmail.com>
2024-06-11 11:56:54 -0400

    Heroku

--- server/dist/utils/class.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Generic class, rename this for any purpose you see fit throughout your application
-Object.defineProperty(exports, "__esModule", { value: true });
-class YourClass {
-}
-exports.default = YourClass;
