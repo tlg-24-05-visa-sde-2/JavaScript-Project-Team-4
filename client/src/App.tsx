540b5a67c63dcd8ba3b75d1b0727dd40d9f62d6c
Kelvin Lau <crosby2987@gmail.com>
2024-06-13 10:06:06 -0500

    About page scaffold

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Home, Login, Signup } from './pages/index';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Home, Login, Signup } from "./pages/index";
+import About from "./pages/About";
 
 function App(): React.ReactElement {
-
-  console.log("production: ", process.env.REACT_APP_PRODUCTION)
+  console.log("production: ", process.env.REACT_APP_PRODUCTION);
   return (
     <Router>
-    <ToastContainer theme="colored" autoClose={2000} />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </Router>
+      <ToastContainer theme="colored" autoClose={2000} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
