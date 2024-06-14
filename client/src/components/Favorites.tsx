0cdd7fb46513257ca1abbf31c2ad3b32531c38f7
Aranka Bullen <131798446+arankabullen@users.noreply.github.com>
2024-06-14 12:39:28 -0400

    resolved favorite icon

--- client/src/components/Favorites.tsx
+++ client/src/components/Favorites.tsx
@@ -2,32 +2,38 @@ import React, { useState, useEffect } from 'react';
 import {Container, Row, Col } from 'react-bootstrap';
 import UserService from '../utils/UserService';
 import ProductCard from '../components/Products/ProductCard';
-
-interface Product {
-  _id: string;
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-}
+//
 
 interface FavoritesProps {
   user: any;
 }
 
 const Favorites: React.FC<FavoritesProps> = ({ user }) => {
-  const [favorites, setFavorites] = useState<Product[]>([]);
-  
+  const [favorites, setFavorites] = useState<any[]>(user?.favorites || []);
+
   useEffect(() => {
     const fetchFavorites = async () => {
-      const userData = await UserService.fetchUserData();
-      if (userData.user) {
-        setFavorites(userData.user.favorites);
+      const data = await UserService.fetchUserData();
+      if (data && data.user && data.user.favorites) {
+        setFavorites(data.user.favorites);
       }
     };
 
     fetchFavorites();
   }, []);
+
+  const handleAddFavorite = async (productId: string) => {
+    const isAlreadyFavorited = favorites.some(product => product._id === productId);
+
+    if (isAlreadyFavorited) {
+      alert('This item is already in your favorites.');
+    } else {
+      const response = await UserService.addFavoriteProduct(productId);
+      if (response.message === 'Product added to favorites') {
+        setFavorites([...favorites, response.product]);
+      }
+    }
+  };
 
   const handleRemoveFavorite = async (productId: string) => {
     await UserService.removeFavoriteProduct(productId);
@@ -37,12 +43,13 @@ const Favorites: React.FC<FavoritesProps> = ({ user }) => {
   return (
     <Container>
       <h1>Favorites</h1>
-      <Row>
-        {favorites.map(product => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+      <Row className="d-flex flex-wrap justify-content-center">
+        {favorites.map((product: any) => (
+          <Col key={product._id} className="d-flex justify-content-center favorite-card">
             <ProductCard
               product={product}
               isFavorite={true}
+              onAddFavorite={handleAddFavorite}
               onRemoveFavorite={handleRemoveFavorite}
             />
           </Col>
--- client/src/components/Products/ProductCard.tsx
+++ client/src/components/Products/ProductCard.tsx
@@ -1,42 +1,45 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-import UserService from '../../utils/UserService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as solidHeart, faHeart as regularHeart } from '@fortawesome/free-solid-svg-icons';
+import "../../assets/css/single-card.css"
 
 interface ProductCardProps {
   product: any;
-  isFavorite?: boolean;
-  onRemoveFavorite?: (productId: string) => void;
+  isFavorite: boolean;
+  onAddFavorite: (productId: string) => void;
+  onRemoveFavorite: (productId: string) => void;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ product, isFavorite = false, onRemoveFavorite }) => {
-  const handleAddFavorite = async () => {
-    await UserService.addFavoriteProduct(product._id);
-    // Optionally, you can update the state to reflect the change
-  };
-
-  const handleRemoveFavorite = async () => {
-    if (onRemoveFavorite) {
-      await UserService.removeFavoriteProduct(product._id);
+const ProductCard: React.FC<ProductCardProps> = ({ product, isFavorite, onAddFavorite, onRemoveFavorite }) => {
+  const handleFavoriteClick = () => {
+    if (isFavorite) {
       onRemoveFavorite(product._id);
+    } else {
+      onAddFavorite(product._id);
     }
   };
 
   return (
-    <Card className="mb-4">
-      <Card.Img variant="top" src={product.image} />
+    <Card className="favorite-card">
+      <Card.Img variant="top" src={product.image} alt={product.name} />
+      <Button
+          variant="link"
+          className="favorite-button"
+          onClick={handleFavoriteClick}
+        >
+          <FontAwesomeIcon icon={isFavorite ? solidHeart : regularHeart} />
+        </Button>
       <Card.Body>
-        <Card.Title>{product.name}</Card.Title>
-        <Card.Text>{product.description}</Card.Text>
-        <Card.Text>${product.price}</Card.Text>
-        {isFavorite ? (
-          <Button variant="danger" onClick={handleRemoveFavorite}>
-            Remove from Favorites
-          </Button>
-        ) : (
-          <Button variant="outline-danger" onClick={handleAddFavorite}>
-            <i className="fa fa-heart"></i> Add to Favorites
-          </Button>
-        )}
+     
+        <Card.Title className="favorite-card-title">{product.name}</Card.Title>
+        <Card.Text className="favorite-card-body">
+          
+          <br />
+          {product.sellersName}
+          <br />
+          ${product.price}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
