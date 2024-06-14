a96c8fc41ffaa5316a582056f1afda20f5965b89
Kelvin Lau <crosby2987@gmail.com>
2024-06-14 11:25:42 -0500

    Fruit and vegetable components added. There are no vegetables in the seed so we for the presentation we can show adding a vegetable. Also capitalized 'jam' in the seed data which also changed the formatting

--- client/src/components/Products/Fruits.tsx
+++ client/src/components/Products/Fruits.tsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ProductService from "../../utils/ProductService";
+import AllCategories from "./AllCategories";
 
-function Fruits(): React.ReactElement {
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  sellersName: string;
+  image: string;
+  _id: string;
+}
+
+interface FruitsProps {
+  props: any; // Ensure products is typed as an array of any
+}
+
+function Fruits({ props }: FruitsProps): React.ReactElement {
+  const [fruits, setFruits] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchFruits = async () => {
+      setLoading(true);
+      try {
+        const response = await ProductService.searchProducts("fruit");
+        setFruits(response.products);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+
+    fetchFruits();
+  }, []);
+
   return (
-    <div>
-      <h1>Fruits</h1>
+    <div className="allProducts-wrapper">
+      <div className="d-flex flex-row">
+        <div className="allProducts-content">
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            <>
+              {fruits.length === 0 ? (
+                <h1>No products found</h1>
+              ) : (
+                <AllCategories products={fruits} props={props} />
+              )}
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
